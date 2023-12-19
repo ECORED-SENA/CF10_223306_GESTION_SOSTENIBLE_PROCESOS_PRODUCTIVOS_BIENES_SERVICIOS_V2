d878e0e6d8c24876447c00e1706c018bba86d8e9
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2023-12-19 11:23:17 -0500

    PDF accesible

--- src/config/global.js
+++ src/config/global.js
@@ -122,7 +122,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF01_223306_DU.pdf',
+        download: 'downloads/CF10_223306_DU.zip',
       },
       {
         icono: 'fas fa-download',
