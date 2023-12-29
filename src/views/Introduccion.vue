a828e7c7115acbe371cd2eeb3e398db22c5cfc45
flizcanor <flizcanor@icloud.com>
2023-12-29 08:30:00 -0500

    se cargan ajustes de repo

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -10,13 +10,13 @@
       h1 Introducción
 
 
-    p Tenga una cordial bienvenida al estudio de este componente formativo denominado Estudio del trabajo y su aplicación en los sistemas de producción. Para avanzar en el contenido de este espacio formativo y los elementos a partir de la estandarización de los procesos productivos según los métodos industriales, observe la información que presenta el siguiente video a manera de introducción:
+    p Tenga una cordial bienvenida al estudio de este componente formativo denominado Estudio del trabajo y su aplicación en los sistemas de producción. Para avanzar en el contenido de este espacio formativo y los elementos a partir de la estandarización de los procesos productivos según los métodos industriales, observe la información que presenta el siguiente video, a manera de introducción:
 
     figure(data-aos="zoom-in")
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/obdiCn6WJuo" title="Nombre del video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video. Estudio del trabajo y su aplicación en los sistemas de producción 
-      
+      figcaption Video. Estudio del trabajo y su aplicación en los sistemas de producción
+
 
 
 
